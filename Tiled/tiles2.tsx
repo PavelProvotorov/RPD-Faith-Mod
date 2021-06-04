e1bf3cf965a4f28de15a6343bacfc5211e5f8afa
PavelProvotorov <pavel.provotorov1998@gmail.com>
2021-06-05 01:04:53 +0300

    Revamp
    - Preparing the game files for a new update for Faith
    - Graphics: new tiles_faith_decoration.png with addition of brand-new graphics
    - Levels: Mock up for the new starting area of the game
    - Music: Changed theme.mp3 and winter.mp3 (Author: Yamakasi Shinori)
    - Other: Slight adjustments to version.json, to give credit to the new contributor
    - Other: Separated main game Assets and Tiled maps into different folders

---
+++ Tiled/tiles2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles2" tilewidth="16" tileheight="16" tilecount="64" columns="16">
+ <image source="../Faith/tiles2.png" width="256" height="64"/>
+</tileset>
---
+++ Tiled/tiles_faith_decoration.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_faith_decoration" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../Faith/tiles_faith_decoration.png" width="512" height="512"/>
+</tileset>
